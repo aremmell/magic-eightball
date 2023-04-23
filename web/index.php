b448274313bebf28618d2db140ee81ef1d5187e9
Ryan M. Lederman <lederman@gmail.com>
2023-04-23 12:37:19 -0600

    add web template

---
+++ web/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>The Magic Eightball</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Ryan M. Lederman <lederman@gmail.com>">
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script type="text/javascript">
+      $(function(){
+        console.log('jQuery: Page loading complete!');
+      });
+    </script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+        html {
+            background: black;
+            color: white;
+        }
+    </style>
+  </head>
+  <body>
+    <div class="8b-container">
+     <p>sup</p>
+    </div>
+  </body>
+</html>
+
+<?php
+
+      
+    
+?>
