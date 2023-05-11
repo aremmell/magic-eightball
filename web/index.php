87731b9cbb6398bb3dc2b8e2554640af48b5264b
Ryan M. Lederman <lederman@gmail.com>
2023-05-11 03:08:19 -0600

    fixing typo in android icon manifest

--- web/index.php
+++ web/index.php
@@ -97,7 +97,7 @@
         <link rel="icon" href="favicon.ico" sizes="any">
         <link rel="icon" href="icon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <link rel="manifest" href="icon.webmanifest">
+        <link rel="manifest" href="icons.webmanifest">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <link rel="stylesheet" href="css/index.css">        
         <script src="js/jquery-3.6.4.min.js"></script>
