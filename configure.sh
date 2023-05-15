7d9811e3dd9da720b6ecd2619ec22544adf49652
Ryan M. Lederman <lederman@gmail.com>
2023-05-15 03:30:54 -0600

    bugfix configure.sh

--- configure.sh
+++ configure.sh
@@ -1,39 +1,5 @@
 #!/bin/bash
 
-check_cmake_version() {
-    local _min_major=3
-    local _min_minor=20
-    local _min_patch=0
-    local _too_old=false
-
-    echo "The minimum required version is $_min_major.$_min_minor.$_min_patch"
-    echo "Checking current version..."
-
-    local _ver_output=$(cmake --version)
-    if [[ $_ver_output =~ [^\d]+([0-9]+)\.([0-9]+)\.([0-9]+) ]]; then
-        local _major=${BASH_REMATCH[1]}
-        local _minor=${BASH_REMATCH[2]}
-        local _patch=${BASH_REMATCH[3]}
-        
-        echo "$(which cmake) = $_major.$_minor.$_patch"
-
-        if [[ $_major -lt $_min_major ]] || [[ $_minor -lt $_min_minor ]] || \
-           [[ $_patch -lt $_min_patch ]]; then
-            $_too_old=true
-            echo "Proceeding to upgrade."
-        else
-            echo "Current cmake is sufficient to build with."
-        fi
-    else
-        $_too_old = true
-        echo "Unable to determine cmake version; proceeding to upgrade."
-    fi
-
-    if [[ $_too_old = true ]]; then
-        update_cmake_from_publisher
-    fi
-}
-
 update_cmake_from_publisher() {
     local _arch=$(uname -m)
     local _url=""
@@ -41,9 +7,9 @@ update_cmake_from_publisher() {
     echo "Machine architecture is: $_arch"
 
     if [[ $_arch = "x86_64" ]]; then
-        $_url="https://github.com/Kitware/CMake/releases/download/v3.26.3/cmake-3.26.3-linux-x86_64.sh"
+        _url="https://github.com/Kitware/CMake/releases/download/v3.26.3/cmake-3.26.3-linux-x86_64.sh"
     elif [[ $_arch = "aarch64" ]]; then
-        $_url="https://github.com/Kitware/CMake/releases/download/v3.26.3/cmake-3.26.3-linux-aarch64.sh"
+        _url="https://github.com/Kitware/CMake/releases/download/v3.26.3/cmake-3.26.3-linux-aarch64.sh"
     else
         echo "No package provided for $_arch. Would need to build from source. Giving up."
         return 1
@@ -64,6 +30,40 @@ update_cmake_from_publisher() {
     else
         echo "Installation failed. Giving up."
         return 1
+    fi
+}
+
+check_cmake_version() {
+    local _min_major=3
+    local _min_minor=20
+    local _min_patch=0
+    local _too_old=false
+
+    echo "The minimum required version is $_min_major.$_min_minor.$_min_patch"
+    echo "Checking current version..."
+
+    local _ver_output=$(cmake --version)
+    if [[ $_ver_output =~ [^\d]+([0-9]+)\.([0-9]+)\.([0-9]+) ]]; then
+        local _major=${BASH_REMATCH[1]}
+        local _minor=${BASH_REMATCH[2]}
+        local _patch=${BASH_REMATCH[3]}
+        
+        echo "$(which cmake) = $_major.$_minor.$_patch"
+
+        if [[ $_major -lt $_min_major ]] || [[ $_minor -lt $_min_minor ]] || \
+           [[ $_patch -lt $_min_patch ]]; then
+            _too_old=true
+            echo "Proceeding to upgrade."
+        else
+            echo "Current cmake is sufficient to build with."
+        fi
+    else
+        _too_old = true
+        echo "Unable to determine cmake version; proceeding to upgrade."
+    fi
+
+    if [[ $_too_old = true ]]; then
+        update_cmake_from_publisher
     fi
 }
 
