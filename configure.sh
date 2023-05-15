af69d0b898a6802204e42aad657f47c1813ee158
Ryan M. Lederman <lederman@gmail.com>
2023-05-15 05:33:41 -0600

    trying a new approach: check cmake version; if >= min for presets, use presets. otherwise use fallback commands.

--- configure.sh
+++ configure.sh
@@ -33,49 +33,133 @@ update_cmake_from_publisher() {
     fi
 }
 
-check_cmake_version() {
-    local _min_major=3
-    local _min_minor=20
-    local _min_patch=0
-    local _too_old=false
-
-    echo "The minimum required version is $_min_major.$_min_minor.$_min_patch"
-    echo "Checking current version..."
+# $1 = the name of a variable to receive
+# an array containing the version segments
+get_cmake_version() {
+    if [[ -z ${1} ]]; then
+        return 1
+    fi
 
     local _ver_output=$(cmake --version)
     if [[ $_ver_output =~ [^\d]+([0-9]+)\.([0-9]+)\.([0-9]+) ]]; then
         local _major=${BASH_REMATCH[1]}
         local _minor=${BASH_REMATCH[2]}
         local _patch=${BASH_REMATCH[3]}
-        
-        echo "$(which cmake) = $_major.$_minor.$_patch"
 
-        if [[ $_major -lt $_min_major ]] || [[ $_minor -lt $_min_minor ]] || \
-           [[ $_patch -lt $_min_patch ]]; then
+        eval $1'=($_major $_minor $_patch)'
+        return 0
+    fi
+
+    return 1
+}
+
+# $1 = version number array from get_cmake_version.
+is_cmake_preset_capable() {
+    local _min_major=3
+    local _min_minor=21
+    local _min_patch=0
+
+    if [[ ${1} -lt $_min_major ]] || [[ ${2} -lt $_min_minor ]] || \
+       [[ ${3} -lt $_min_patch ]]; then
+        return 1
+    fi
+
+    return 0
+}
+
+# $1 = version number array from get_cmake_version.
+print_cmake_version() {
+    echo "${1}.${2}.${3}"
+}
+
+print_cmake_info() {
+    _cur_ver=()
+    get_cmake_version _cur_ver
+    
+    echo -n "cmake = $(which cmake), and its version is $(print_cmake_version ${_cur_ver[@]})"
+
+    if is_cmake_preset_capable "${_cur_ver[@]}"; then
+        echo " (preset capable)"
+    else
+        echo " (NOT preset capable)"
+    fi 
+}
+
+update_cmake_if_required() {
+    local _too_old=false
+
+    echo "Checking current version..."
+
+    _cur_ver=();
+    if get_cmake_version _cur_ver; then
+        echo "Current version is $(print_cmake_version ${_cur_ver[@]})"    
+        if ! is_cmake_preset_capable; then
             _too_old=true
-            echo "Proceeding to upgrade."
-        else
-            echo "Current cmake is sufficient to build with."
         fi
     else
         _too_old = true
-        echo "Unable to determine cmake version; proceeding to upgrade."
     fi
 
     if [[ $_too_old = true ]]; then
+        echo "Current version is too old; updating..."
         if ! update_cmake_from_publisher; then
             return 1
         fi
     fi
 }
 
+build_magic_eightball() {
+    # if cmake is new enough (>= 3.20.0), using the preset commands
+    # will work. If cmake is older, we will have to figure out a workaround.
+    print_cmake_info
+    
+    local _use_presets=false
+    if is_cmake_preset_capable ${_cur_ver[@]}; then
+        _use_presets=true
+        echo "cmake is new enough to proceed with preset commands..."
+    else
+        echo "cmake is too old to proceed with preset commands; have to" \
+             " revert to older behavior..."
+    fi
+
+    if [[ ${_use_presets} = true ]]; then
+        echo "=== Available presets ==="
+        cmake --list-presets
+        echo "========================="
+
+        cmake --debug-output --fresh --preset release && \
+        cmake --debug-output --build --preset release
+    else
+        # the CMake Tools extension will print out the commands
+        # that *should* work without the presets file. Let's just try those.
+
+        # I have seen one particular cmake complain that the build directory doesn't exist.
+        # The current version also complains about --target being and unknown command, but it
+        # is clearly explained in the documentation.
+        mkdir -p build/install
+
+        # this is configure
+        cmake --debug-output --fresh -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=build/install \
+              --check-system-vars -S$(pwd) -B$(pwd)/build -G Ninja && \
+        # this is build
+        cmake --debug-output --verbose  --build $(pwd)/build --clean-first --target magic-eightball 
+    fi
+
+    [[ -x "build/magic-eightball" ]] && build/magic-eightball -q "Do you think this will work?"
+}
+
 _args=($@)
 case ${_args[0]} in
-    "check-cmake")
-        check_cmake_version
+    #"update-cmake")
+     #   update_cmake_if_required
+      #  ;;
+    "cmake-info")
+        print_cmake_info
+        ;;
+    "build")
+        build_magic_eightball
         ;;
     *)
-        echo "Usage: $0 [check-cmake]"
+        echo "Usage: $0 [cmake-info|build]"
         ;;
 esac
-
