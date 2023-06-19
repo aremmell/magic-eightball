ef50d302d432995aa7f5e9396a590eaeb2e128c3
Ryan M. Lederman <lederman@gmail.com>
2023-06-18 19:32:44 -0600

    make url to og:image absolute. TODO: https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/summary

--- web/index.php
+++ web/index.php
@@ -60,7 +60,7 @@
         <meta property="og:description" content="<?php echo($show_question ? ($appState->get_question_value() . "&nbsp;" . LOC_OG_DESC_PERMALINK) : LOC_MAGIC_EIGHTBALL_DESC); ?>">
         <meta property="og:url" content="<?php echo($show_question ? $appState->get_permalink() : "https://rml.dev/magic-eightball") ?>">
         <meta property="og:type" content="website">
-        <meta property="og:image" content="/img/og-image.png">
+        <meta property="og:image" content="https://rml.dev/magic-eightball/img/og-image.png">
 
         <link rel="icon" href="favicon.png">
         <link rel="icon" href="favicon.ico" sizes="any">
